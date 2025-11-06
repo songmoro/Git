6bebec5f70da67690dfc237826e539accf1b62b6
songmoro <wlqgkr@naver.com>
2025-11-06 10:17:12 +0900

    Initial Commit

---
+++ AProject/AProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AProjectApp.swift
+//  AProject
+//
+//  Created by 송재훈 on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct AProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AProject
+//
+//  Created by 송재훈 on 11/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
